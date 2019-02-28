2e71f58d560e4b80a787aaf72cba0127af6dfc88
celestialbunny <coronateh@outlook.com>
2019-02-28 19:57:32 +0800

    update google

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Blueprint
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
@@ -26,6 +27,12 @@ users_blueprint = Blueprint('users',
 from flask_dance.contrib.github import make_github_blueprint, github
 github_blueprint = make_github_blueprint(client_id='f2bbc19d909797496071', client_secret='3be3e6bd1126a19456e72a1d94882f855b6228f2')
 app.register_blueprint(github_blueprint, url_prefix='/github_login')
+
+# Google Auth
+from authlib.client import OAuth2Session
+import google.oauth2.credentials
+import googleapiclient.discovery
+from oauth2client.file import Storage
 
 @users_blueprint.route('/github')
 def github_login():
@@ -206,3 +213,55 @@ def logout():
 	logout_user()
 	flash("You've been logged out!", "success")
 	return redirect(url_for('users.index'))
+
+"""
+Google Auth
+"""
+ACCESS_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
+AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent'
+
+AUTHORIZATION_SCOPE ='openid email profile'
+
+AUTH_TOKEN_KEY = 'auth_token'
+AUTH_STATE_KEY = 'auth_state'
+USER_INFO_KEY = 'user_info'
+
+@users_blueprint.route('/google/login')
+def google_login():
+	session = OAuth2Session(app.config['GOOGLE_CLIENT_ID'], app.config['GOOGLE_CLIENT_SECRET'], scope=app.config['GOOGLE_AUTHORIZATION_SCOPE'], redirect_uri=url_for('users.index'))
+	uri, state = session.authorization_url(AUTHORIZATION_URL)
+	flask.session[AUTH_STATE_KEY] = state
+	session.permanent = True
+	return redirect(uri, code=302)
+
+@users_blueprint.route('/google/auth')
+def google_auth_redirect():
+	state = request.args.get('state', default=None, type=None)
+	session = OAuth2Session(app.config['GOOGLE_CLIENT_ID'], app.config['GOOGLE_CLIENT_SECRET'], scope=app.config['GOOGLE_AUTHORIZATION_SCOPE'], state=state, redirect_uri=url_for('users.index'))
+	oauth2_tokens = session.fetch_access_token(ACCESS_TOKEN_URI, authorization_response=request.url)
+	flask.session[AUTH_TOKEN_KEY] = oauth2_tokens
+	return redirect(url_for('users.index'))
+
+def build_credentials():
+	oauth2_tokens = session[AUTH_TOKEN_KEY]
+	return google.oauth2.credentials.Credentials(
+		oauth2_tokens['access_token'],
+		refresh_token=oauth2_tokens['refresh_token'],
+		client_id=app.config['GOOGLE_CLIENT_ID'],
+		client_secret=app.config['GOOGLE_CLIENT_SECRET'],
+		token_uri=ACCESS_TOKEN_URI
+	)
+
+def get_user_info():
+	credentials = build_credentials()
+	oauth2_client = googleapiclient.discovery.build('oauth2', 'v2', credentials=credentials)
+	return oauth2_client.userinfo().get().execute()
+
+def store_user_info():
+	user = get_user_info()
+	storage = Storage()
+	storage.put(user)
+	# How to determine whether the user has signed up with us?
+	# How to save the user credentials inside the database?
+	credentials = storage.get()
+	# What is the purpose of the line above?? For login?
